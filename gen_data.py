d1c853a3b9c5c659554875c8b071555664fbe68a
rafdp <twrafdp@ya.ru>
2019-04-16 16:41:10 +0300

    Added shuffling and normalization

--- gen_data.py
+++ gen_data.py
@@ -4,8 +4,8 @@ import translate.storage.tmx as tmx
 from load_hparams import *
 import numpy as np
 import os
-
-
+import operator
+import string
 
 CheckHparamsMandatory("source_lang")
 CheckHparamsMandatory("target_lang")
@@ -21,6 +21,7 @@ target_lang = hparams["target_lang"]
 exp_path = hparams["data_path"] + experiment + '/'
 if not os.path.isdir(exp_path + 'dg'):
     os.makedirs(exp_path + 'dg')
+
 ######################
 ######### Corpus work
 ######################
@@ -32,12 +33,59 @@ with open(hparams["data_path"] + source_lang + '-' + target_lang + '.tmx', "rb")
 units = corpus.getunits()
 print(len(units))
 print("  rearranging")
-source_corpus = [unit.getNodeText(unit.getlanguageNode(source_lang)) 
-                 for unit in units]
-target_corpus = [unit.getNodeText(unit.getlanguageNode(target_lang)) 
-                 for unit in units]
+source_corpus = np.array([unit.getNodeText(unit.getlanguageNode(source_lang)) 
+                          for unit in units])
+target_corpus = np.array([unit.getNodeText(unit.getlanguageNode(target_lang)) 
+                          for unit in units])
 
+removal_rate = 0.05
+if "char_removal%" in hparams:
+    removal_rate = hparams["char_removal%"]/100.0
 
+print("  normalizing (char removal rate %d %%)" % int(removal_rate*100))
+
+def Normalize(corpus, rate):
+    chars = {}
+    
+    for i in range(len(corpus)):
+        corpus[i] = corpus[i].lower().translate(str.maketrans('', '', string.punctuation))
+        for c in corpus[i]:
+            if c not in chars:
+                chars[c] = 1
+            else :
+                chars[c] += 1
+    
+    sorted_chars = sorted(chars.items(), key=operator.itemgetter(1))
+    cum_sum = []
+    prev_sum = 0
+    for i in range(len(sorted_chars)):
+        cum_sum.append(prev_sum + chars[sorted_chars[i][0]])
+        prev_sum = cum_sum[i]
+    cut = 0
+    for i in range(len(cum_sum)):
+        if cum_sum[i] > (1-rate)*prev_sum:
+            cut = i
+            break
+    delete_chars = ''.join([c[0] for c in sorted_chars[cut:]])
+    
+    for s in corpus:
+        s = s.replace(delete_chars, '')
+    return (corpus, cut, len(cum_sum))
+
+source_corpus, source_cut, source_count = Normalize(source_corpus, removal_rate)
+print("    input: removed %d of %d characters" % (source_count - source_cut, source_count))
+target_corpus, target_cut, target_count = Normalize(target_corpus, removal_rate)
+print("    output: removed %d of %d characters" % (target_count - target_cut, source_count))
+
+print("  shuffling")
+rand_state = np.random.get_state()
+np.random.shuffle(source_corpus)
+np.random.set_state(rand_state)
+np.random.shuffle(target_corpus)
+
+print("Test strings:")
+print("  input: '%s'" % source_corpus[0])
+print("  output: '%s'" % target_corpus[0])
 
 ######################
 ######### Vocab work
@@ -52,11 +100,11 @@ def GenerateVocab(path, corpus):
         print("    vocab found")
         return te.SubwordTextEncoder(path)
     else:
-        print("    building..")
+        print("    building")
         encoder = te.SubwordTextEncoder.\
                         build_from_generator(corpus,
                                    target_size=hparams['vocab_size'])
-        print("    storing..")
+        print("    storing")
         encoder.store_to_file(path)
         return encoder
     
